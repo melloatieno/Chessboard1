26c4c4b6436cb799e44ea1f2cbc7c8b4d511bbcd
melloh <atienomelvine98@gmail.com>
2022-06-01 13:04:40 +0300

    The initial commit for Rock-Paper-Scissor

---
+++ Rock-Paper-Scissor.py
@@ -0,0 +1,36 @@
+import random 
+
+print ("Winning rule of the Rock-Paper-Scissor game ar as follows: \n Same choice-> draw \n Rock vs paper->paper wins \n Rock vs scissor->Rock wins \n paper vs scissor->scissor wins \n")
+
+while True:
+   user_choice = (input("User turn (R, P, S):"))
+   possible_actions = ["R", "P", "S"]
+   computer_selection = random.choice(possible_actions)
+
+   print("Computer choice is: " + computer_selection)
+   print(user_choice + "  vs  " + computer_selection)
+
+   if user_choice == computer_selection:
+       print("Both players selected {user_choice}. It's a tie")
+   elif user_choice == "R":
+       if computer_selection == "S":
+        print("Rock smashes scissors! You win!")
+       else:
+        print("Paper covers rock! You lose!")
+   elif user_choice == "P":
+       if computer_selection == "R":
+        print ("Paper covers rock! You win")
+       else:
+        print("Scissors cuts paper! You lose!")
+   elif user_choice == "S":
+       if computer_selection == "P":
+        print ("Scissors cuts paper! You win!")
+       else:
+        print("Rock smashes scissors! You lose")
+
+   print("Do you want to play again? (Y/N)")
+   ans = input()
+   if ans == 'n' or ans == 'N':
+        break
+
+print("\nThanks for playing")
